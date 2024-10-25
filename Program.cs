1a7847a9f41eccff360d3a3c278f8e074101a0fc
Davy <machadodavi3778@gmail.com>
2024-10-25 13:01:39 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+public class exercicio1
+{
+    public static void Main(string[] args)
+    {
+        //declarar variaveis
+        int num1 = 10;
+        int num2 = 20;
+
+        int soma = num1 + num2;
+        int mult = num1 * num2;
+        int div = num1 / num2;
+        int sub = num1 - num2;
+        int rest = num1 % num2;
+
+
+
+        Console.WriteLine(soma);
+        Console.WriteLine(mult);
+        Console.WriteLine(div);
+        Console.WriteLine(sub);
+        Console.WriteLine(rest);
+
+
+
+
+    }
+
+}
+
+
